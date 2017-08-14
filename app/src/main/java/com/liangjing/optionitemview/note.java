e04dac0d0a172bdfec3cb51222fa638d105bfe2e
梁竞 <1184106223@qq.com>
2017-08-14 09:49:27 +0800

    Update note.java

--- app/src/main/java/com/liangjing/optionitemview/note.java
+++ app/src/main/java/com/liangjing/optionitemview/note.java
@@ -11,5 +11,18 @@ public class note {
      * 水平居中很简单,paint.setTextAlign(Align.CENTER);然后drawText的x坐标设置为rect.centerX()即可(rect是可绘制的范围)，
      * 竖直居中稍显麻烦，可以使用FontMetrics对象计算文字高度，然后计算baseline，使文字垂直居中
      */
+    
+      /**
+       *  return true 无法触发点击事件；return false时无法发生滑动 ; 
+       *  return super.onTouchEvent(event);才能触发这两个事件
+       *    为什么?
+       *   return super.onTouchEvent(event);
+       *
+       *   OnClickListener.onClick()是在原生的View.onTouchEvent()方法里面回调的, 你自己重写了这个方法, 
+       *    而且不调用super.onTouchEvent(event)的话当然就不会回调onClick()方法了.
+       */
+         
+       
+       
 
 }
